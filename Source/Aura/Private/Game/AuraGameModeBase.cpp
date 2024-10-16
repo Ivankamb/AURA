2aa6a22f8f8a5e26d89aa192a836784b6a13bcbc
Kambulov <kia290@mail.ru>
2024-10-16 05:41:20 +0300

    Add movement to Aura

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Created by KIA
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Created by KIA
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
